a3d1688d5889e27a8bf02b20a5a83b63978b0c63
marspere <1587393449@qq.com>
2019-12-05 19:41:47 +0800

    add basic log

---
+++ export.go
@@ -0,0 +1,23 @@
+package logify
+
+import "fmt"
+
+// Debug log DEBUG level message.
+func Debug(v interface{}) {
+	fmt.Println(baseLog.defaultFormatLog(debugFormat, v, baseLog.CallerSkipDepth))
+}
+
+// Warn log WARN level message.
+func Warn(v interface{}) {
+	fmt.Println(baseLog.defaultFormatLog(warnFormat, v, baseLog.CallerSkipDepth))
+}
+
+// Info log INFO level message.
+func Info(v interface{}) {
+	fmt.Println(baseLog.defaultFormatLog(infoFormat, v, baseLog.CallerSkipDepth))
+}
+
+// Error log ERROR level message.
+func Error(v interface{}) {
+	fmt.Println(baseLog.defaultFormatLog(errorFormat, v, baseLog.CallerSkipDepth))
+}
---
+++ log.go
@@ -0,0 +1,55 @@
+package logify
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Logs struct {
+	Prefix           string // prefix of each line when printing
+	Level            int    // log level, above level will be exported to file
+	EnableCallerSkip bool   // whether to enable print function call stack ascend
+	CallerSkipDepth  int    // the depth of ascend
+}
+
+const (
+	infoFormat        = "%s [INFO] [%s] %v"
+	debugFormat       = "%s [DEBUG] [%s] %v"
+	warnFormat        = "%s [WARN] [%s] %v"
+	errorFormat       = "%s [ERROR] [%s] %v"
+	defaultTimeFormat = "2006-01-02 15:04:05"
+)
+
+var baseLog = newLogs()
+
+func newLogs() *Logs {
+	return &Logs{
+		EnableCallerSkip: true,
+		CallerSkipDepth:  2,
+	}
+}
+
+func (l *Logs) defaultFormatLog(format string, v interface{}, skip int) string {
+	_, file, line, ok := runtime.Caller(skip)
+	if ok {
+		location := parseFilename(file) + ":" + strconv.FormatInt(int64(line), 10)
+		return fmt.Sprintf(format, time.Now().Format(defaultTimeFormat), location, v)
+	}
+	return fmt.Sprintf(parseFormat(format), time.Now().Format(defaultTimeFormat), v)
+}
+
+func parseFilename(filename string) string {
+	str := strings.Split(filename, "/")
+	if len(str) > 1 {
+		return str[len(str)-1]
+	}
+	return str[0]
+}
+
+func parseFormat(format string) string {
+	str := strings.Split(format, " ")
+	return str[0] + " " + str[1] + " " + str[3]
+}
