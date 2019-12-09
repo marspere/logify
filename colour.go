ff2c5488b38d12f0d14c705391cadc3b06699470
marspere <1587393449@qq.com>
2019-12-09 11:34:37 +0800

    log colour printing

---
+++ colour.go
@@ -0,0 +1,30 @@
+package logify
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	infoColour  = "%c[1;40;34m%s%c[0m"
+	debugColour = "%c[1;40;32m%s%c[0m"
+	warnColour  = "%c[1;40;33m%s%c[0m"
+	errorColour = "%c[1;40;31m%s%c[0m"
+)
+
+func parseOutput(output string) string {
+	if strings.Contains(output, "[ERROR]") {
+		str := strings.Split(output, "[ERROR]")
+		return str[0] + fmt.Sprintf(errorColour, 0x1B, "[ERROR]", 0x1B) + str[1]
+	}
+	if strings.Contains(output, "[WARN]") {
+		str := strings.Split(output, "[WARN]")
+		return str[0] + fmt.Sprintf(warnColour, 0x1B, "[WARN]", 0x1B) + str[1]
+	}
+	if strings.Contains(output, "[DEBUG]") {
+		str := strings.Split(output, "[DEBUG]")
+		return str[0] + fmt.Sprintf(debugColour, 0x1B, "[DEBUG]", 0x1B) + str[1]
+	}
+	str := strings.Split(output, "[INFO]")
+	return str[0] + fmt.Sprintf(infoColour, 0x1B, "[INFO]", 0x1B) + str[1]
+}
--- export.go
+++ export.go
@@ -4,20 +4,20 @@ import "fmt"
 
 // Debug log DEBUG level message.
 func Debug(v interface{}) {
-	fmt.Println(baseLog.defaultFormatLog(debugFormat, v, baseLog.CallerSkipDepth))
+	fmt.Println(parseOutput(baseLog.defaultFormatLog(debugFormat, v, baseLog.CallerSkipDepth)))
 }
 
 // Warn log WARN level message.
 func Warn(v interface{}) {
-	fmt.Println(baseLog.defaultFormatLog(warnFormat, v, baseLog.CallerSkipDepth))
+	fmt.Println(parseOutput(baseLog.defaultFormatLog(warnFormat, v, baseLog.CallerSkipDepth)))
 }
 
 // Info log INFO level message.
 func Info(v interface{}) {
-	fmt.Println(baseLog.defaultFormatLog(infoFormat, v, baseLog.CallerSkipDepth))
+	fmt.Println(parseOutput(baseLog.defaultFormatLog(infoFormat, v, baseLog.CallerSkipDepth)))
 }
 
 // Error log ERROR level message.
 func Error(v interface{}) {
-	fmt.Println(baseLog.defaultFormatLog(errorFormat, v, baseLog.CallerSkipDepth))
+	fmt.Println(parseOutput(baseLog.defaultFormatLog(errorFormat, v, baseLog.CallerSkipDepth)))
 }
