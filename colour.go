96fd9ec8a840a52afae18887c063b3f22bf5c1c0
marspere <1587393449@qq.com>
2019-12-17 10:19:47 +0800

    fix unbuild bug under the linux environment

--- colour.go
+++ colour.go
@@ -3,6 +3,7 @@ package logify
 import (
 	"fmt"
 	"strings"
+	"syscall"
 )
 
 const (
@@ -10,6 +11,13 @@ const (
 	debugColour = "%c[1;40;32m%s%c[0m"
 	warnColour  = "%c[1;40;33m%s%c[0m"
 	infoColour  = "%c[1;40;34m%s%c[0m"
+)
+
+const (
+	infoColor  = 1 | 1
+	warnColor  = 2 | 8
+	debugColor = 3 | 1
+	errorColor = 4 | 4
 )
 
 func parseOutput(output string) string {
@@ -27,4 +35,40 @@ func parseOutput(output string) string {
 	}
 	str := strings.Split(output, "[INFO]")
 	return str[0] + fmt.Sprintf(infoColour, 0x1B, "[INFO]", 0x1B) + str[1]
+}
+
+func parsePrint(output string) {
+	if strings.Contains(output, "[ERROR]") {
+		callWindowsPrint(output, "[ERROR]", errorColor)
+		return
+	}
+	if strings.Contains(output, "[WARN]") {
+		callWindowsPrint(output, "[WARN]", warnColor)
+		return
+	}
+	if strings.Contains(output, "[DEBUG]") {
+		callWindowsPrint(output, "[DEBUG]", debugColor)
+		return
+	}
+	callWindowsPrint(output, "[INFO]", infoColor)
+}
+
+func callWindowsPrint(output, level string, colour int) {
+	str := strings.Split(output, level)
+	fmt.Print(str[0])
+	kernel32 := syscall.NewLazyDLL("kernel32.dll")
+	proc := kernel32.NewProc("SetConsoleTextAttribute")
+	handle, _, _ := proc.Call(uintptr(syscall.Stdout), uintptr(colour))
+	fmt.Print(level)
+	handle, _, err := proc.Call(uintptr(syscall.Stdout), uintptr(7))
+	if err != nil {
+		fmt.Print(str[1] + "\n")
+		return
+	}
+	_, _, err = kernel32.NewProc("CloseHandle").Call(handle)
+	if err != nil {
+		fmt.Print(str[1] + "\n")
+		return
+	}
+	fmt.Print(str[1] + "\n")
 }
